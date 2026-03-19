5a31a788d8c753d3fb4948470d5ca605434c50b3
JuliaPalacios <julia.pal.r@gmail.com>
2026-03-18 17:23:28 -0700

    Adjust lower bound for probability calculation

--- R/bounded_coal_functions.R
+++ R/bounded_coal_functions.R
@@ -277,7 +277,7 @@ coal_loglik_bounded = function(init, f)
     
     Lambda <- sum(sllnocoal)
     bound_prob <- sum(r_ntip * exp(-com_vec * Lambda))
-    if (bound_prob<0){bound_prob<-1e-8}
+    if (bound_prob<0){bound_prob<-1e-16}
     print("bound prob")
     print(bound_prob)
     ll_vec <- -init$y * f - llnocoal 
