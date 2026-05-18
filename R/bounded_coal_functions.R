a26cddbce21b5ffa7cd456a7c5c1f86bf6488d37
JuliaPalacios <julia.pal.r@gmail.com>
2026-05-17 17:25:22 -0700

    Update bounded_coal_functions.R

--- R/bounded_coal_functions.R
+++ R/bounded_coal_functions.R
@@ -295,8 +295,8 @@ coal_loglik_bounded = function(init, f)
     
     Lambda <- sum(sllnocoal)
     bound_prob <- sum(r_ntip * exp(-com_vec * Lambda))
-    if (bound_prob<0.001){
-       bound_prob<-.001
+    if (bound_prob<0.01){
+       bound_prob<-.01
        #print(Lambda)
        #break
     }
