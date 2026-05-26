f917e40e63edcb246139c22e0d8c4abb74ae39d4
JuliaPalacios <julia.pal.r@gmail.com>
2026-05-25 18:47:35 -0700

    Enhance ESS2 with variable arguments and add ESS_wrapper
    Added variable arguments support to ESS2 function and defined ESS_wrapper function.

--- R/Phyloinfer.R
+++ R/Phyloinfer.R
@@ -50,7 +50,7 @@ ESS = function(q_cur, l_cur, loglik, cholC, ...)
   return(list(q=q, u=l, Ind=1))
 }
 
-ESS2 = function(q_cur, l_cur, loglik, kappa, lik_init)
+ESS2 = function(q_cur, l_cur, loglik, kappa, lik_init, ...)
 {  
   # choose ellipse
   nu=sim_rw1(lik_init$args$grid, signal = 1/kappa, constraint ="anchor", anchor_index = 1) 
@@ -91,6 +91,7 @@ ESS2 = function(q_cur, l_cur, loglik, kappa, lik_init)
   
   return(list(q=q, u=l, Ind=1))
 }
+
 ESS_wrapper = function(lik_init, loglik, l_cur, f, kappa, cholC, invC, alpha, beta)
 {
   #ll = function(f) coal_loglik(init = lik_init, f = f)
